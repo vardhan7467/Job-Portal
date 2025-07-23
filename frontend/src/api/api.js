ccf5b295cd113d84c663cb6311e5a066bdfb135f
vardhan7467 <vardhan.12015326@lpu.in>
2025-07-23 21:11:26 +0530

    Update API URL in frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://job-portal-backend.onrender.com/api',
 });
+
+
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
